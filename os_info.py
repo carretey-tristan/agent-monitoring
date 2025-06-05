08b00a1fa79e2cae7b6cce638e86337bcee47111
carretey-tristan <carretey.tristan@proton.me>
2025-06-05 15:33:18 +0200

    Delete os_info.py

--- os_info.py
+++
@@ -1,9 +0,0 @@
-from platform import system,version
-from socket import gethostname
-
-def get_os_info():
-    return {
-        'hostname': gethostname(),
-        'os': system(),
-        'os_version': version()
-    } 
